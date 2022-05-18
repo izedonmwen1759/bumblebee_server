a823dee748e3032bb935fbc9dcea204c6c26209c
izedonmwen1759 <izedonmwenpeter@gmail.com>
2022-05-18 16:44:29 -0700

    make it better

--- app.js
+++ app.js
@@ -1,70 +1,62 @@
 const express = require('express')
-const res = require('express/lib/response')
-const path = require('path')
 const app = express()
 const PORT = process.env.PORT ||  5006
 const server = app.listen(PORT, () => console.log('server running on '+PORT))
 const io = require('socket.io')(server, {
     cors:{
-        origin:'*'
+        origin:'*'      
     }
 }) 
-
-const { use } = require('express/lib/application')
-const req = require('express/lib/request')
-const { redirect } = require('express/lib/response')
-const fetch = require("node-fetch") 
-const { response } = require('express')
+const fetch = require("node-fetch")
 //app.use(express.static(path.join(__dirname, 'testwork')))
 
 let socketConnected = new Set()
 var users = []
-
 io.on('connection',  onConnected) 
 
 function onConnected(socket){
- console.log(socket.id)
-    socketConnected.add(socket.id)
+  
+    console.log(socket.id)
+    socketConnected.add(socket.id)  
+ 
     socket.on('user_connected', (id) =>{
-        users[id] = socket.id       
-        socket.emit('user_connect', id)
+        users[id] = socket.id
+        socket.broadcast.emit("user_connected", id) 
+       
     })
 
-    socket.on('login-details', (res) =>{
+function userstatus(id){
+    var userSocket = users[id]
+    return userSocket
+}
+
+    socket.on('userstatus', (data)=>{        
+       
+        if(userstatus(data.otheruser) === undefined){
+            var res = false
+        }else{
+            var res = true
+        }
+        socket.emit('userstatus', res) 
+        console.log(userstatus(data.otheruser))
+        console.log(res)
+    })
+     socket.on('login-details', (res) =>{   
         const url = 'https://sm.emarkets24.com/etalk_app/newFile/login.php'  
        var data = JSON.stringify(res) 
         fetch(url, {
-            method: 'POST',
+            method: 'POST',      
             body: data,
-            mode:'cors' 
-        }).then(response => response.text())
+            mode:'cors'  
+        }).then(response => response.text())      
         .then(res => {   
-            var data = JSON.parse(res)
+            var data = JSON.parse(res)  
            // console.log(data)
-           socket.emit('userlogin', data)
+           socket.emit('userlogin', data) 
         }) 
     })
     
-    socket.on("user_connected", (user) =>{        
-        //save in array        
-        users[user] = socket.id
-       
-        const url = 'https://sm.emarkets24.com/bumblebee/php/updatelogin.php'
-        const d = {
-            userid:user,
-            num:1
-        }
-        const data = JSON.stringify(d)
-        fetch(url, {
-            method: 'POST',
-            body: data,
-            mode:'cors'
-        }).then(response => response.text())
-        .then(data => {
-           // console.log(data)
-           // socket.emit('report', data)
-        }); 
-    })
+
     socket.on('find-messengers', (res) =>{
         const url = 'https://sm.emarkets24.com/etalk_app/newFile/messengers.php'  
         var data = {
@@ -83,24 +75,44 @@ function onConnected(socket){
             socket.emit('displayMessenges', res)
          })
     })
-    
-    socket.on('message', (res) =>{
-        var socketId = users[res.to]  
-        //var usersocket = users[res.from]      
-        io.to(socketId).emit('chat-message', res) 
-        io.to(socketId).emit('lastmessage', res.message,res.from)  
-        var data = JSON.stringify(res)
+        
+    socket.on('message', (info) =>{
+        console.log(info.message)
+        var socketId = users[info.to]
+        socket.broadcast.to(socketId).emit('chat-message', info)
+       socket.broadcast.to(socketId).emit('lastmessage', info.message,info.from)
+         if(info.type === '0'){
+            var data = JSON.stringify(info) 
          const url = 'https://sm.emarkets24.com/etalk_app/newFile/chatMessages.php'              
          fetch(url, {
              method: 'POST',
-             body: data,
-             mode:'cors'
+             body: data,  
+             mode:'cors'    
          }).then(response => response.text())
-         .then(response => {             
+         .then(response => {               
          })     
-       
-    }) 
-    socket.on('allchat messages', (res) =>{
+
+        }else if(info.type === 1 || info.type === '1'){
+           
+         } else{} 
+                
+    })  
+    socket.on('user-profile', (id)=>{
+        const info ={
+            userid:id
+        }    
+        var data = JSON.stringify(info) 
+        const url = 'https://sm.emarkets24.com/etalk_app/newFile/checkoutprofile.php' 
+        fetch(url, {
+            method: 'POST',
+            body: data,  
+            mode:'cors'    
+        }).then(response => response.text())
+        .then(response => { 
+            socket.emit('user-profile', response)              
+        })   
+    })
+    socket.on('allchat messages', (res) =>{   
         var data = JSON.stringify(res)
          const url = 'https://sm.emarkets24.com/etalk_app/newFile/openmessages.php'              
          fetch(url, {
@@ -109,8 +121,9 @@ function onConnected(socket){
              mode:'cors'
          }).then(response => response.text())
          .then(response => {   
-        socket.emit('messages', response)
-         })  
+       socket.emit('messages', response)   
+         })   
+            
     }) 
     socket.on('find number', (dat) =>{
         const res = {
@@ -142,9 +155,27 @@ function onConnected(socket){
         }   
        
     })
+
+    socket.on('task', (res)=>{
+        var data = JSON.stringify(res) 
+        const url = 'https://sm.emarkets24.com/etalk_app/newFile/tasks.php'              
+        fetch(url, {
+            method: 'POST',
+            body: data,
+            mode:'cors'
+        }).then(response => response.text())
+        .then(response => {   
+            console.log(response) 
+            socket.emit('task', response)     
+        })  
+      
+    })
    // socket.broadcast.emit('brdcast', socket.id)
 
     io.emit('clients-total', socketConnected.size)   
+    socket.on('disconnect', () => {
+    console.log('Socket disconnected', socket.id)
+    socketConnected.delete(socket.id)        
+    })
 
-
-}
+} 
